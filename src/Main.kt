8e9929dc11d05a6bbed7ca1cbcb56c3b4dc9bcc5
PedroHMurdiga <murdigapedro@gmail.com>
2026-08-03 11:05:30 -0300

    Exercicios 1

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,20 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
+    var n1 = 12;
+    val n2: Int = n1;
+    n1 = 55;
+
+    println("Hello World");
+    println(n1);
+    println(n2);
+
+    //===============
+    //Leitura de dados
+    println("Digite sua idade:");
+    var idade = readln().toInt();
+
+    idade++;
+    println("No ano que vem sua idadde será:" + idade);
 
 }
